de0f3627d3573115756592a3e868470bd6ac5fcb
ZWLori <lori961120@gmail.com>
2018-07-04 23:18:56 +0800

    Change to corresponding name in stage3

--- app/www/js/stage3.js
+++ app/www/js/stage3.js
@@ -1,15 +1,11 @@
-
-/* For feedback slider */
-var slider = document.getElementById("myRange");
-var output = document.getElementById("rangeValue");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-    output.innerHTML = this.value;
-}
-
 function store_rating(){
-    sessionStorage.setItem('rating', slider.value);
+    // TODO obtain the real value
+    sessionStorage.setItem('rating', 1);
     document.location.href = './stage4.html';
 }
+
+if (sessionStorage.getItem('convStyle') == 'dominant')
+    $('#roboName').text("Max")
+else
+    $('#roboName').text("Linus")
+    
